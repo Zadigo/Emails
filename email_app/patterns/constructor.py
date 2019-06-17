2d3960c683ead3059eaebe83e3a1897a847c2b29
John <pendenquejohn@gmail.com>
2019-06-17 09:49:43 +0200

    Refactoring

--- email_app/patterns/constructor.py
+++ email_app/patterns/constructor.py
@@ -21,9 +21,11 @@ class FileOpener(UtilitiesMixin):
         # Pop the headers but keep
         # them for later usage
         self.headers = csv_content.pop(0)
+
         for content in csv_content:
             for i in range(len(content)):
                 content[i] = self.normalize_name(content[i])
+                
         # Store the csv's content
         self.csv_content = csv_content
 
