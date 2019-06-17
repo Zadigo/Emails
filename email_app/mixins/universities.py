1d4eb670446c3238ec180f76b4bf311739c85daf
John <pendenquejohn@gmail.com>
2019-06-17 10:02:01 +0200

    Created unique module

--- email_app/mixins/universities.py
+++
@@ -1,8 +0,0 @@
-from email_app.patterns.patterns import NamePatterns
-from email_app.mixins.fields import EmailField
-from email_app.core.settings import Configuration
-
-class Universities(NamePatterns):
-    pattern = None
-
-Universities(Configuration()['DUMMY_FILE']).construct_pattern()
